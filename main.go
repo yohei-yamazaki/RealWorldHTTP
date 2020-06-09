23a6f3d4dd25f3d5cfbb7eb4582b221ee7c076af
yohei-yamazaki <y.y.takkyuu.love@gmail.com>
2020-06-09 23:02:54 +0900

    use proxy

--- main.go
+++ main.go
@@ -3,27 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/cookiejar"
 	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
-	jar, err := cookiejar.New(nil)
+	proxyUrl, err := url.Parse("http://localhost:18888")
 	if err != nil {
 		panic(err)
 	}
 	client := http.Client{
-		Jar: jar,
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
 	}
-	for i := 0; i < 2; i++ {
-		resp, err := client.Get("http://localhost:18888/cookie")
-		if err != nil {
-			panic(err)
-		}
-		dump, err := httputil.DumpResponse(resp, true)
-		if err != nil {
-			panic(err)
-		}
-		log.Println(string(dump))
+	resp, err := client.Get("http://github.com")
+	if err != nil {
+		panic(err)
 	}
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
 }
